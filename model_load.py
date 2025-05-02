6c523bb18a460aaafd1315399825031ef7dfc53e
Manasip13 <pandavmanasi13@gmail.com>
2025-05-02 16:35:07 +0530

    Update model_load.py
    mistralai/Mistral-7B-Instruct-v0.3

--- model_load.py
+++ model_load.py
@@ -18,7 +18,8 @@ def get_response(user_input):
 
         completion = client.chat.completions.create(
             # model="google/gemma-2-2b-it",
-            model="google/gemma-3-1b-it",
+            # model="google/gemma-3-1b-it",
+            model="mistralai/Mistral-7B-Instruct-v0.3"
             messages=messages,
             max_tokens=500
         )
